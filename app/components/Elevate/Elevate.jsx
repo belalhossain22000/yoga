33a4dfdc481a1fc21a61641f764bcd7db48551c6
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-09-01 20:52:55 +0600

    make responsive elevent section

--- app/components/Elevate/Elevate.jsx
+++ app/components/Elevate/Elevate.jsx
@@ -5,18 +5,21 @@ import circle1 from './image/Ellipse 2.png';
 import circle2 from './image/Ellipse 3.png';
 import person from './image/young-woman-doing-natarajasana-exercise 1.png';
 import Button from '@/app/utils/Button/Button';
+import Container from '@/app/utils/Container/Container';
 
 const Elevate = () => {
   return (
-    <section className='xlg:mt-[150px] mt-10 flex items-center justify-between small-container b-red-600'>
+    <Container maxWidth={"max-w-[1090px]"}>
+
+    <section className='mt-[150px] mt- flex xm:flex-row flex-col items-center justify-between gap-5 px-5 xl:px-0 b-red-600'>
       {/* left side */}
-      <div className='relative'>
+      <div className='relative w-[350px] xm:w-[400px] h-full b-red-500' >
         <Image src={circle1} alt='circle'/>
-        <Image className='absolute bottom-0 left-10 right-0' src={person} alt='persons'/>
+        <Image className='absolute bottom-0 left-8 right-0 h-[110%] w-auto' src={person} alt='persons'/>
         <Image src={circle2} alt='circle'/>
       </div>
       {/* right side */}
-      <div className='flex flex-col items-start gap-[30px]'>
+      <div className='flex flex-col items-start gap-3 xlg:gap-[30px]'>
         <h2>Elevate your being: <br /> transform body,  <br />
           mind, life.</h2>
           <p>Are you ready to embark on a life-altering journey of self- <br />transformation? Get ready to unleash your true potential and <br /> experience a profound shift in your body, mind, and overall <br /> quality of life. Heres how you can make it happen:</p>
@@ -25,6 +28,7 @@ const Elevate = () => {
           </Button>
       </div>
     </section>
+    </Container>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
      <Navbar/>
      <Hero/>
      <Category/>
-     {/* <Elevate/> */}
+     <Elevate/>
      {/* <Videos/> */}
      {/* <Place/> */}
      {/* <Teachers/> */}
